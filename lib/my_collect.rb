841c99a9563a8854cc8129aecfaab4f9f7a37c9c
Akili Humphrey <akilihumphrey@gmail.com>
2018-11-06 10:01:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|\
+yield(collection[lang])
+  lang.upcase
+end
 
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |lang| "#{lang.upcase}" }
